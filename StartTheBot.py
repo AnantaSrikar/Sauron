2f808e3b574ef38d93445c871b8a2bce6820f58d
Ananta Srikar <srikarananta01@gmail.com>
2020-03-28 16:20:03 +0530

    Added basic functionality

--- StartTheBot.py
+++ StartTheBot.py
@@ -14,6 +14,31 @@ def getTokens():
 def start(update, context):
 	update.message.reply_text("Hey there {}! I'm still awake".format(update.message.from_user.mention_markdown()), parse_mode = markdown)
 
+
+def databaseUpdates(update, context):
+
+	if(update.message.text.startswith('#infected')):
+		infectionData = update.message.text.split(' ')
+		if(len(infectionData) != 4):
+			update.message.reply_text('Invalid format, please try again')
+		else:
+			replyText = ''
+			replyText += 'City : {}\n'.format(infectionData[1])
+			replyText += 'Count : {}\n'.format(infectionData[2])
+			replyText += 'Link : {}\n'.format(infectionData[3])
+			update.message.reply_text(replyText)
+	
+	elif(update.message.text.startswith('#death')):
+		deathData = update.message.text.split(' ')
+		if(len(infectionData) != 4):
+			update.message.reply_text('Invalid format, please try again')
+		else:
+			replyText = ''
+			replyText += 'City : {}\n'.format(deathData[1])
+			replyText += 'Count : {}\n'.format(deathData[2])
+			replyText += 'Link : {}\n'.format(deathData[3])
+			update.message.reply_text(replyText)
+
 def main():
 
 	getTokens()
@@ -24,6 +49,9 @@ def main():
 	dispatcher = updater.dispatcher
 
 	dispatcher.add_handler(CommandHandler('start', start))
+	dispatcher.add_handler(MessageHandler(Filters.text, databaseUpdates))
+
+	print('Bot started running!')
 
 	updater.start_polling() # Starts the bot
 
